999714dc2791e87ca284267965ec171fb8aecf45
VeveJana <sevcik.jana@gmail.com>
2025-03-24 08:57:36 +0100

    Update secondory_table_creation.sql

--- secondory_table_creation.sql
+++ secondory_table_creation.sql
@@ -2,8 +2,8 @@ create table t_jana_veverkova_project_SQL_secondary_final as
 select
 	cp.payroll_year,
 	e.gdp,
-    avg(cp.value) as average_wages,
-    avg(cp2.value) as food_price
+	avg(cp.value) as average_wages,
+	avg(cp2.value) as food_price
 from czechia_payroll cp
 join economies e on e.year = cp.payroll_year
 	and e.country = 'Czech Republic'
@@ -14,3 +14,5 @@ where cp.value_type_code = 5958
 	and cp.value is not null
 group by cp.payroll_year, e.gdp
 order by cp.payroll_year;
+
+

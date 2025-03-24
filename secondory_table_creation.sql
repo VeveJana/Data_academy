bb6fb0b4313125b91f70827b3f2e2c3198e3ce42
VeveJana <sevcik.jana@gmail.com>
2025-03-24 09:12:33 +0100

    Update secondory_table_creation.sql

--- secondory_table_creation.sql
+++ secondory_table_creation.sql
@@ -1,18 +1,17 @@
-create table t_jana_veverkova_project_SQL_secondary_final as
-select
-	cp.payroll_year,
-	e.gdp,
-	avg(cp.value) as average_wages,
-	avg(cp2.value) as food_price
-from czechia_payroll cp
-join economies e on e.year = cp.payroll_year
-	and e.country = 'Czech Republic'
-left join czechia_price cp2
-	on date_part ('year', cp2.date_from) = cp.payroll_year
-where cp.value_type_code = 5958
-	and cp2.value is not null
-	and cp.value is not null
-group by cp.payroll_year, e.gdp
-order by cp.payroll_year;
-
-
+CREATE TABLE t_jana_veverkova_project_SQL_secondary_final AS
+SELECT
+    cp.payroll_year,
+    e.gdp,
+    AVG(cp.value) AS average_wages,
+    AVG(cp2.value) AS food_price
+FROM czechia_payroll cp
+JOIN economies e 
+    ON e.year = cp.payroll_year
+    AND e.country = 'Czech Republic'
+LEFT JOIN czechia_price cp2
+    ON DATE_PART('year', cp2.date_from) = cp.payroll_year
+WHERE cp.value_type_code = 5958
+    AND cp2.value IS NOT NULL
+    AND cp.value IS NOT NULL
+GROUP BY cp.payroll_year, e.gdp
+ORDER BY cp.payroll_year;

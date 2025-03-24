0b332c7d5e279db2f02bc560542bd14e731e6e5d
VeveJana <sevcik.jana@gmail.com>
2025-03-24 09:11:34 +0100

    Update primary_table_creation.sql

--- primary_table_creation.sql
+++ primary_table_creation.sql
@@ -1,21 +1,21 @@
-create table t_jana_veverkova_project_SQL_primary_final as
-select 
+CREATE TABLE t_jana_veverkova_project_SQL_primary_final AS
+SELECT 
     cp.payroll_year,
-    cp.value as average_wages,
-    cpib.name as industry_name,
-    cp2.value as food_price,
+    cp.value AS average_wages,
+    cpib.name AS industry_name,
+    cp2.value AS food_price,
     cp2.category_code,
-    cpc.name as food_category_name,
+    cpc.name AS food_category_name,
     cpc.price_value,
     cpc.price_unit
-from czechia_payroll cp
-join czechia_payroll_industry_branch as cpib
-    on cp.industry_branch_code = cpib.code
-left join czechia_price as cp2
-    on date_part('year', cp2.date_from) = cp.payroll_year
-    and date_part('quarter', cp2.date_from) = cp.payroll_quarter
-left join czechia_price_category cpc
-    on cp2.category_code = cpc.code
-where cp.value_type_code = 5958  
-order by
+FROM czechia_payroll cp
+JOIN czechia_payroll_industry_branch AS cpib
+    ON cp.industry_branch_code = cpib.code
+LEFT JOIN czechia_price AS cp2
+    ON DATE_PART('year', cp2.date_from) = cp.payroll_year
+    AND DATE_PART('quarter', cp2.date_from) = cp.payroll_quarter
+LEFT JOIN czechia_price_category cpc
+    ON cp2.category_code = cpc.code
+WHERE cp.value_type_code = 5958  
+ORDER BY
     cp.payroll_year;
